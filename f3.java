ed27efa71a609c30faa0cf2878b5040e24b57a9c
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-03-08 16:29:09 +0530

    Create f3.java

---
+++ f3.java
@@ -0,0 +1,36 @@
+class f1
+{
+  public static void main (String[]args)
+  {
+	//integer datatype:::    
+	int a = 43;
+	byte b = 87;
+	long c = 56666666L;//define ong in last with L....
+	short d = 45;
+
+	//decimal:::
+	float e = 5.675f;//here define f becuse f means float
+	double f = 2.6543212;
+
+	//Character
+	char g = 'd';
+
+	//String
+	String h = "yourname";
+
+	boolean i = true;
+
+	  System.out.println(a);
+	  System.out.println(b);
+	  System.out.println(c);
+      System.out.println(d);
+      System.out.println(e);
+      System.out.println(f);
+      System.out.println(g);
+      System.out.println(h);
+      System.out.println(i);
+
+
+
+  }
+}
