cd317e2efc61f45620c69c889750e5c2043e5f05
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-03-08 21:29:02 +0530

    Create f4.java

---
+++ f4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class f4
+{
+	public static void main(String[] args) {
+		Scanner input= new Scanner(System.in);
+		
+		System.out.print("enter number 1:");
+		int a= input.nextInt();
+		
+		System.out.print("enter number 2:");
+		int b= input.nextInt();
+		
+		int sum=a+b,sub=a-b,multi=a*b,div=a/b;
+		
+		System.out.println("the sumation of these number are :"+sum);
+		System.out.println("the substraction of these number are :"+sub);
+		System.out.println("the multiplication of these number are :"+multi);
+		System.out.println("the division of these number are :"+div);
+		
+	}
+}
