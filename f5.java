664d8737d3024ee5fc2b72098b95b25b99c05f42
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-03-09 12:26:47 +0530

    Update f5.java

--- f5.java
+++ f5.java
@@ -3,6 +3,9 @@ class f5
 {
 	public static void main(String[] args) {
 	   	Scanner input= new Scanner(System.in);
+	   	
+	   	System.out.print("enter your name:");
+	   	String name=input.nextLine();
 		
 		System.out.print("enter marks of subject 1:");
 		int sub1= input.nextInt();
@@ -20,6 +23,6 @@ class f5
 		int sub5= input.nextInt();
 		
 	    float percentage = ((sub1+sub2+sub3+sub4+sub5)/500.0f)*100;
-	    System.out.println("the percentage::"+percentage);
+	    System.out.println("congratulation " +name+ " you got "+percentage+" percentage.");
 	}
 }
