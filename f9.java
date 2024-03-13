fc384c4c9b2f401cf11f1a5d6439cb8c1ddd7ef3
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-03-13 22:11:21 +0530

    Create f9.java

---
+++ f9.java
@@ -0,0 +1,20 @@
+class f9
+{
+  public static void main (String[]args)
+  {
+
+	int a = 23;
+	  System.out.print (a);
+	int b = a++;				//first print the value then increment
+	  System.out.print ("\n");
+	  System.out.print (b);
+
+    System.out.print ("\n");
+
+	int x = 34;
+	  System.out.print (x);
+	int y = ++x;				//first increment then print the value
+	  System.out.print ("\n");
+	  System.out.print (y);
+  }
+}
