0b452b72d0343a84d71ac0f8b06acba088bde864
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-03-14 10:25:34 +0530

    Update f9.java

--- f9.java
+++ f9.java
@@ -2,19 +2,22 @@ class f9
 {
   public static void main (String[]args)
   {
-
+System.out.print("increment operator\n");
 	int a = 23;
 	  System.out.print (a);
 	int b = a++;				//first print the value then increment
 	  System.out.print ("\n");
 	  System.out.print (b);
+          System.out.print("\n");
+          System.out.print(a);
 
-    System.out.print ("\n");
-
+    System.out.print ("\ndecrement operator\n");
 	int x = 34;
 	  System.out.print (x);
 	int y = ++x;				//first increment then print the value
 	  System.out.print ("\n");
 	  System.out.print (y);
+	  System.out.print ("\n");
+	  System.out.print (x);
   }
 }
