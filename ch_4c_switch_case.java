9bed66e98c083a06deccc700f95d403b2083dabd
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-03-16 21:38:37 +0530

    Create ch_4c_switch_case.java

---
+++ ch_4c_switch_case.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class ch_4c_switch_case
+{
+  public static void main (String[]args)
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter Number between 1 to 7:");
+    int Number=sc.nextInt();
+    
+    switch(Number){
+       case 1:
+           System.out.println("monday");
+           break;
+       case 2:
+           System.out.println("tuesday");
+           break;
+       case 3:
+           System.out.println("wednesday");
+           break;
+        case 4:
+            System.out.println("thrusday");
+            break;
+        case 5:
+           System.out.println("friday");
+           break;
+        case 6:
+           System.out.println("saturday");
+           break;
+        case 7:
+           System.out.println("sunday");
+           break;
+        default:
+         System.out.println("default");
+         break;
+  }}}
