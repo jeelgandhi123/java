ae94068edd08f375eb3fd0e09185a6a0485ea32b
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-03-16 17:10:20 +0530

    Create ch_4c_if_elseif_else.java

---
+++ ch_4c_if_elseif_else.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class ch_4c_if_elseif_else
+{
+  public static void main (String[]args)
+  {
+	Scanner sc=new Scanner(System.in);
+	System.out.print("enter number 1:");
+	int num1=sc.nextInt();
+	
+	System.out.print("enter number 2:");
+	int num2=sc.nextInt();
+	
+	System.out.print("enter number 3:");
+	int num3=sc.nextInt();
+	
+	if(num1>num2){
+	    if(num1>num3){
+	        System.out.println(num1+" is greatest");
+	    }
+	    else{
+	         System.out.println(num3+" is greatest");
+	    }
+	}
+	else
+	    if(num2>num3){
+	        System.out.println(num2+" is greatest");
+	    }
+	    else{
+	         System.out.println(num3+" is greatest");
+	    }
+	}}
