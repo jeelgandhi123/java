832789b168ee2e36d2951b29872ad33ffa4e664e
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-03-16 14:58:59 +0530

    Create f12.java

---
+++ f12.java
@@ -0,0 +1,25 @@
+class f12
+{
+	public static void main(String[] args) {
+    //problem1.....
+    String name="JEEL";
+    System.out.println(name.toLowerCase());
+    
+    //problem2....
+    String myname="jeel gandhi hemi gandhi";
+    System.out.println(myname.replace(" ","_"));
+    
+    //problem3..
+    String letter="Dear </name/> thanks a lot for helping me!!";
+    System.out.println(letter.replace("</name/>","viratkohli"));
+    
+    //problem4......
+    String justname="vipul gandhi  jyotika gandhi   ";
+    System.out.println(justname.indexOf("  "));
+    System.out.println(justname.indexOf("   "));
+    
+    //problem5.....
+    String myletter="dear sir\n\ttoday i am absent because of my health is not good.\nthanks!!";
+    System.out.println(myletter);
+	}
+}
