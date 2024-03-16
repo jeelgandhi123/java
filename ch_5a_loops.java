249ef6eed851c321fecba101251b69a4d74b085a
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-03-16 23:09:15 +0530

    Create ch_5a_loops.java

---
+++ ch_5a_loops.java
@@ -0,0 +1,25 @@
+class ch_5a_loops
+{
+  public static void main (String[]args)
+  {   
+    System.out.print("loops.....\n");
+    
+    int i=1;
+    while(i<3){
+        System.out.print("using while loop:"+i+"\n");
+        i++;
+    }
+    
+    
+    int a=1;
+    do{
+        System.out.print("using do-while loop:"+a+"\n");
+        a++;
+    }while(a<3);
+    
+    
+    for(int b=1;b<3;b++){
+        System.out.print("using for loop:"+b+"\n");
+    }
+  }
+}
