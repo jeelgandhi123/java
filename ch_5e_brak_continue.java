88941a70ea8e07c9fa18a5fab70cb8e68d9c75bd
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-03-17 15:27:15 +0530

    Create ch_5e_brak_continue.java

---
+++ ch_5e_brak_continue.java
@@ -0,0 +1,22 @@
+class ch_5e_brak_continue
+{
+	public static void main(String[] args) {
+		System.out.println("break and continue....");
+		
+		for(int a=0;a<4;a++){
+		    System.out.println(a);
+		    //now use break here....
+		    if(a==1){
+		     System.out.println("i will end this loop using break..");
+		     break;
+		   }}
+	
+	   for(int b=0;b<4;b++){
+	       if(b==2){
+	           System.out.println("i will use continue statment here...");
+	           continue;
+	       }
+	       System.out.println(b);
+	   }
+}
+}
