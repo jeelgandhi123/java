e73fc3c953ed935507714de369deb12189fbe0a3
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-03-19 22:47:38 +0530

    Create ch_6b_multidimension.java

---
+++ ch_6b_multidimension.java
@@ -0,0 +1,20 @@
+class ch_6b_multidimension
+{
+	public static void main(String[] args) {
+		System.out.println("multi-dimension arrays:");
+		
+		int[][] matrix =new int [2][2];
+		   
+		matrix[0][0]=23;
+		matrix[0][1]=90;
+		matrix[1][0]=89;
+		matrix[1][1]=78;
+		
+		for(int i=0; i<matrix.length ;i++){
+		    for(int j=0;j<matrix[i].length;j++){
+		        System.out.printf("%d ",matrix[i][j]);
+		    }
+		    System.out.print("\n");
+		}
+	}
+}
