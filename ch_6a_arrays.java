c55dcc0c19e92097237baee95961fb41b11529b5
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-03-20 16:37:05 +0530

    Update ch_6a_arrays.java

--- ch_6a_arrays.java
+++ ch_6a_arrays.java
@@ -1,16 +1,18 @@
 class ch_6a_arrays
 {
 	public static void main(String[] args) {
-		System.out.println("arrays.....");
+	System.out.println("arrays.....");
 		
 		//how arrays works.....
 		int[] marks=new int[5];
 		
+		//int [] marks={34,45,56,76,79};
+		
 		marks[0]=34;
 		marks[1]=45;
-	    marks[2]=54;
-	    marks[3]=69;
-	    marks[4]=00;
+	        marks[2]=54;
+	        marks[3]=69;
+	        marks[4]=00;
 	    
 		System.out.println(marks[0]);
 		System.out.println(marks[1]);
@@ -21,15 +23,21 @@ class ch_6a_arrays
 		//marks.length....
 		System.out.println("it gives the length of array:"+marks.length);
 		
-		//prnting using for loop
+		//printing using for loop
 		for(int i=0;i<marks.length;i++){
 		    System.out.println(marks[i]);
 		}
 		
-		 System.out.println("printing string in reverse order...");
+		System.out.println("printing marks in reverse order...");
 		 
 		for(int j=marks.length-1;j>=0;j--){
            System.out.println(marks[j]);
 		}
+		
+		System.out.println("printing using for each loop:");
+		for(int element:marks){
+		    System.out.println(element);
+		}
+	
 	}
 }
