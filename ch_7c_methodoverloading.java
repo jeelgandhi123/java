e16237267fed2a89ddd8a1a12fff8d8b7454a764
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-03-25 14:50:45 +0530

    Create ch_7c_methodoverloading.java

---
+++ ch_7c_methodoverloading.java
@@ -0,0 +1,27 @@
+class ch_7c_methodoverloading
+{
+    static void multi(){
+        System.out.println("example");
+    }
+    static int multi(int a,int b){
+        int c=a+b;
+        return c;
+        
+    }
+    static int multi(int a,int b, int c){
+        int d=a+b+c;
+        return d;
+    }
+	public static void main(String[] args) {
+	 System.out.println("method overloading!! the method overloading is that the multiple method have same name...");
+	 
+	 System.out.println("--------------------------");
+	 multi();   
+	 
+	 int x=multi(34,78,56);
+	 System.out.println(x);
+	 
+	 int y=multi(56,76);
+	 System.out.println(y);
+	}
+}
