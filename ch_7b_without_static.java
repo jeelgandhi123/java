e936a6ce2f78eebf10414da869acb37ef1160bfc
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-03-25 14:16:22 +0530

    Create ch_7b_without_static.java

---
+++ ch_7b_without_static.java
@@ -0,0 +1,17 @@
+class ch_7b_without_static
+{
+    int sum(int a,int b){
+       int c;
+        c=a+b;
+        return c;
+}
+
+public static void main(String[] args) {
+        int a=3,b=99,c;
+        //classname obj=new classname()
+        Main obj = new Main();
+        //obj.methodname
+        c= obj.sum(a,b);
+        System.out.println("the sum of "+a+" and "+b+" is :" +c);
+    }
+}
