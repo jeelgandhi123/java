40687a440807676ad8d5e53681c5a2d02519d233
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-03-25 14:14:56 +0530

    Create ch_7a_method_static.java

---
+++ ch_7a_method_static.java
@@ -0,0 +1,15 @@
+class ch_7a_method_static
+{
+    static int sum(int x,int y){
+        int z;
+        z=x+y;
+        return z;
+        
+    }
+    public static void main (String[]args)
+    {   
+        int a=90,b=80,c;
+        c=sum(a,b);
+        System.out.print("the sum of "+a+" and "+b+" is :" +c);
+  }
+}
