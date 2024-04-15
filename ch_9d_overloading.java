eb947968d9800c52d0bc6e1a7ea4dc4524080344
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-04-15 18:16:02 +0530

    Create ch_9d_overloading.java

---
+++ ch_9d_overloading.java
@@ -0,0 +1,43 @@
+class collage{
+    int student_id;
+    String student_name;
+    
+    public collage(){
+        //class name and the constructor name must be same....
+        student_id=90;
+        student_name="hello!!";
+    }
+    public collage(int id){
+        student_id=id;
+        student_name="none!!";
+    }
+    public collage(int id,String name){
+        student_id=id;
+        student_name=name;
+    }
+
+    public int print_student_id(){
+        return student_id;
+    }
+    public String print_student_name(){
+        return student_name;
+    }
+}
+class ch_9d_overloading
+{
+	public static void main(String[] args) {
+		/*collage jeel=new collage();
+	    System.out.println("my id no is:"+jeel.print_student_id());
+	    System.out.println("my name is:"+jeel.print_student_name());*/
+	    
+	   // collage jeel=new collage(67);
+	   // System.out.println("my id no is:"+jeel.print_student_id());
+	   // System.out.println("my name is:"+jeel.print_student_name());
+	    
+	    collage jeel=new collage(89,"rahul modi");
+	     System.out.println("my id no is:"+jeel.print_student_id());
+	    System.out.println("my name is:"+jeel.print_student_name());
+	    
+	    
+	}
+}
