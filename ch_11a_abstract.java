cb0194529c3f3d9087701a70da541cd3613499ec
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-06-16 22:22:17 +0530

    Update ch_11a_abstract.java

--- ch_11a_abstract.java
+++ ch_11a_abstract.java
@@ -11,10 +11,15 @@ class child extends parent{
     }
 }
 
-class ch_11a_absract {
+class ch_11a_abstract {
     public static void main(String[] args) {
        child c1=new child();
        c1.call();
        c1.love();
+       
+       System.out.println("2nd method!!!!");
+       parent c2=new child();
+       c2.call();
+       c2.love();
     }
 }
